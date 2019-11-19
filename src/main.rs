9f6cbb6010b7f2c161c8226773318641fb32adf1
Nicolas Mohr <Nico.Mohr@gmx.net>
2019-11-19 13:59:12 +0100

    added basic actix web server

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use serde::Deserialize;
 use std::error::Error;
 
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
 
 // Example
 // Goal, Target, Indicator, SeriesCode, SeriesDescription, GeoAreaCode, GeoAreaName, TimePeriod, Value, Time_Detail,  UpperBound, LowerBound, BasePeriod, Source, FootNote, Nature, Units,[Reporting Type]
 // "6","6.3","6.3.2","EN_H2O_OPAMBQ","Proportion of open water bodies with good ambient water quality (%)","40","Austria","2017","91.94","2013-2015","","","","Environment Live","","C","PERCENT","G"
 
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 #[serde(rename_all = "PascalCase")] 
 struct Record {
     goal: String,
@@ -19,7 +21,7 @@ struct Record {
     geo_area_name: String,
     time_period: String,
     value: f64,
-    time_Detail: String,
+    time_detail: String,
     upper_bound: String,
     lower_bound: String,
     base_period: String,
@@ -30,18 +32,40 @@ struct Record {
     reporting_type: Option<String>,
 }
 
-
+struct AppState {
+    records: Vec<Record>,
+}
 
 fn main() {
 
-    match parse_data() {
-        Ok(records) => records.iter().for_each(|r| println!("{:?}: {:?}", r.geo_area_name, r.value)),
-        Err(e) => {
-            println!("error parsing data: {}", e);
-            std::process::exit(1);
-        }
-    }
+    let rec = match parse_data() {
+        Ok(r) => r,
+        Err(_e) => Vec::new(),
+    };
 
+    // records.iter().for_each(|r| println!("{:?}: {:?}", r.geo_area_name, r.value))
+
+    HttpServer::new(move || {
+        App::new()
+            .data(AppState {
+                records: rec.clone(),
+            })
+            .route("/", web::get().to(index))
+            .route("/data", web::get().to(data))
+    })
+    .bind("127.0.0.1:8088")
+    .unwrap()
+    .run()
+    .unwrap();
+}
+
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+fn data(data: web::Data<AppState>) -> impl Responder {
+    let records = &data.records;
+    HttpResponse::Ok().body(&records[0].geo_area_name)
 }
 
 
